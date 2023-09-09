5e0cf33940a4a27ba8d90dc0f73adaa63f29f7cc
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2023-09-09 13:51:37 +0100

    Practice with React-bootsrap and jsx

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const product = {
+  name:` T-shirt `,
+ 
+  description:`Upgrade your wardrobe with our classic cotton T-shirt. 
+    Made from 100% premium cotton.`,
+ 
+    price: 4000, 
+};
+
+export default product;
